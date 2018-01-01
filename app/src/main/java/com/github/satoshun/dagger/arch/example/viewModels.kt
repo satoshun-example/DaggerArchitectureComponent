c79050e579b4994850414e9878e3e5042ef54659
SatoShun <shun.sato1@gmail.com>
2018-01-01 11:51:57 +0000

    remove redundant classC

--- app/src/main/java/com/github/satoshun/dagger/arch/example/viewModels.kt
+++ app/src/main/java/com/github/satoshun/dagger/arch/example/viewModels.kt
@@ -2,6 +2,7 @@ package com.github.satoshun.dagger.arch.example
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.FragmentActivity
 import dagger.Module
 import dagger.Provides
@@ -18,8 +19,14 @@ abstract class ActivityProviderModule<in T : FragmentActivity, VM : ViewModel>(
   fun providedActivity(activity: T): FragmentActivity = activity
 
   @Provides
-  fun provideViewModel(creator: ViewModelCreator<VM>): VM = creator()
+  fun provideClass(): Class<VM> = kclass
 
   @Provides
-  fun provideClass(): Class<VM> = kclass
+  fun provideViewModel(
+      activity: FragmentActivity,
+      kclass: Class<VM>,
+      factory: ViewModelInjectorFactory<VM>
+  ): VM {
+    return ViewModelProviders.of(activity, factory).get(kclass)
+  }
 }
